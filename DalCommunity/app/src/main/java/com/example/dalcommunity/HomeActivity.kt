8afd70912d24d59d466250e39365677446b693b0
Tijil Parmar <tparmar@dal.ca>
2024-03-15 10:14:48 -0300

    chore: Upload initial project for the team to work on.

---
+++ DalCommunity/app/src/main/java/com/example/dalcommunity/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.example.dalcommunity
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val window: Window = this.window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.background)
+        window.navigationBarColor = ContextCompat.getColor(this, R.color.background)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val posts = listOf("", "", "")
+
+        var adapter = HomeAdapter(this, posts)
+        recyclerView.adapter = adapter
+    }
+}
