ab291ec54778df162783a2fe1e9b500ecf920f5a
Prithvi Manoj Krishna <pr732042@dal.ca>
2024-03-20 00:06:57 -0300

    Add basic profile details editing layout and activity

---
+++ DalCommunity/app/src/main/java/com/csci5708/dalcommunity/activity/ProfileDetailActivity.kt
@@ -0,0 +1,35 @@
+package com.csci5708.dalcommunity.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dalcommunity.R
+
+class ProfileDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val profileDetailToolbar: Toolbar = findViewById(R.id.profile_detail_toolbar)
+        setSupportActionBar(profileDetailToolbar)
+        supportActionBar?.title = "Edit Profile"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home)
+            finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
