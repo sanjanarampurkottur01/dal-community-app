2ac478fc32569d93512f57bc8f51ff92a0b7f605
Prithvi Manoj Krishna <pr732042@dal.ca>
2024-03-23 20:44:52 -0300

    Add log out functionality

---
+++ DalCommunity/app/src/main/java/com/csci5708/dalcommunity/constants/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.csci5708.dalcommunity.constants
+
+class AppConstants {
+    companion object {
+        const val APP_SHARED_PREFERENCES = "sharedPref"
+        const val SP_IS_SIGNED_IN_KEY = "isSignedIn"
+    }
+}
--- DalCommunity/app/src/main/java/com/csci5708/dalcommunity/fragment/ProfileFragment.kt
+++ DalCommunity/app/src/main/java/com/csci5708/dalcommunity/fragment/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.csci5708.dalcommunity.fragment
 
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,10 +11,14 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.csci5708.dalcommunity.activity.AccountSettingsActivity
+import com.csci5708.dalcommunity.activity.HomeActivity
 import com.csci5708.dalcommunity.activity.ProfileDetailActivity
 import com.csci5708.dalcommunity.activity.TempActivity
 import com.csci5708.dalcommunity.activity.UserPostsActivity
+import com.csci5708.dalcommunity.constants.AppConstants
 import com.example.dalcommunity.R
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 /**
  * A simple [Fragment] subclass.
@@ -31,18 +36,23 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
+        val sharedPreferences =
+            activity?.getSharedPreferences(AppConstants.APP_SHARED_PREFERENCES, MODE_PRIVATE)
+
         val profileEditButton: ImageView = view.findViewById(R.id.profile_page_edit_button)
         profileEditButton.setOnClickListener {
             val profileDetailIntent = Intent(activity, ProfileDetailActivity::class.java)
             activity?.startActivity(profileDetailIntent)
         }
 
-        val accountSettingsImg: ImageView = view.findViewById(R.id.profile_page_account_settings_option)
+        val accountSettingsImg: ImageView =
+            view.findViewById(R.id.profile_page_account_settings_option)
         accountSettingsImg.setOnClickListener {
             val accountSettingsIntent = Intent(activity, AccountSettingsActivity::class.java)
             activity?.startActivity(accountSettingsIntent)
         }
-        val accountSettingsTxt: TextView = view.findViewById(R.id.profile_page_account_settings_text)
+        val accountSettingsTxt: TextView =
+            view.findViewById(R.id.profile_page_account_settings_text)
         accountSettingsTxt.setOnClickListener {
             val accountSettingsIntent = Intent(activity, AccountSettingsActivity::class.java)
             activity?.startActivity(accountSettingsIntent)
@@ -62,6 +72,18 @@ class ProfileFragment : Fragment() {
             // TODO: REMOVE TEMP ACTIVITY BEFORE SUBMISSION
 //            val intent = Intent(activity, TempActivity::class.java)
 //            activity?.startActivity(intent)
+            if (sharedPreferences != null) {
+                if (sharedPreferences.getBoolean(AppConstants.SP_IS_SIGNED_IN_KEY, false)) {
+                    val currentUser = Firebase.auth.currentUser
+                    if (currentUser != null) {
+                        Firebase.auth.signOut()
+                        sharedPreferences.edit().putBoolean(AppConstants.SP_IS_SIGNED_IN_KEY, false)
+                            .apply()
+                    }
+                    val intent = Intent(activity, HomeActivity::class.java)
+                    activity?.startActivity(intent)
+                }
+            }
         }
 
         return view
