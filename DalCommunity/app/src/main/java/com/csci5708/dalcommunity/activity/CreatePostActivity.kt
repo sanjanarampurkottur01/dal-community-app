6c0c8ff66281579800c306cbd7020ea0e6969275
Sanjana Rampur Kottur <sn929443@dal.ca>
2024-03-26 18:24:03 -0300

    Return location from GetLocationActivity to CreatePostActivity

--- DalCommunity/app/src/main/java/com/csci5708/dalcommunity/activity/CreatePostActivity.kt
+++ DalCommunity/app/src/main/java/com/csci5708/dalcommunity/activity/CreatePostActivity.kt
@@ -1,11 +1,20 @@
+// CreatePostActivity.kt
 package com.csci5708.dalcommunity.activity
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.view.WindowManager
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.dalcommunity.R
 
 class CreatePostActivity : AppCompatActivity() {
@@ -13,8 +22,11 @@ class CreatePostActivity : AppCompatActivity() {
     private lateinit var btnToChooseFromGallery: ImageView
     private lateinit var btnToClickPicture: ImageView
     private lateinit var btnToGetLocation: ImageView
+    private lateinit var postCaption: EditText
+    private lateinit var locationTextView: TextView
 
     private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_GET_LOCATION = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +35,10 @@ class CreatePostActivity : AppCompatActivity() {
         btnToChooseFromGallery = findViewById(R.id.image_gallery_icon)
         btnToClickPicture = findViewById(R.id.image_camera_icon)
         btnToGetLocation = findViewById(R.id.image_location_icon)
+        postCaption = findViewById(R.id.post_content_edit_text)
+
+        postCaption.requestFocus()
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
 
         btnToChooseFromGallery.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -35,9 +51,27 @@ class CreatePostActivity : AppCompatActivity() {
         }
 
         btnToGetLocation.setOnClickListener {
-            val getLocationIntent = Intent(this, GetLocationActivity::class.java)
-            startActivity(getLocationIntent)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Permission is not granted, request it
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_GET_LOCATION
+                )
+            } else {
+                // Permission has already been granted, start accessing location
+                startGetLocationActivity()
+            }
         }
+    }
+
+    private fun startGetLocationActivity() {
+        val getLocationIntent = Intent(this, GetLocationActivity::class.java)
+        startActivityForResult(getLocationIntent, REQUEST_GET_LOCATION)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -45,6 +79,12 @@ class CreatePostActivity : AppCompatActivity() {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             val imageUri = data?.data
             // Load the selected image into imageView or do whatever you want with it
+        } else if (requestCode == REQUEST_GET_LOCATION && resultCode == Activity.RESULT_OK) {
+            val latitude = data?.getDoubleExtra("latitude", 0.0)
+            val longitude = data?.getDoubleExtra("longitude", 0.0)
+
+            Log.e("RETVAL", latitude.toString())
+            Log.e("RETVAL", latitude.toString())
         }
     }
 }
--- DalCommunity/app/src/main/java/com/csci5708/dalcommunity/activity/GetLocationActivity.kt
+++ DalCommunity/app/src/main/java/com/csci5708/dalcommunity/activity/GetLocationActivity.kt
@@ -1,21 +1,23 @@
+// GetLocationActivity.kt
 package com.csci5708.dalcommunity.activity
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.csci5708.dalcommunity.constants.AppConstants.Companion.LOCATION_PERMISSION_REQUEST_CODE
 import com.example.dalcommunity.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
 class GetLocationActivity : AppCompatActivity() {
-    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,8 +54,10 @@ class GetLocationActivity : AppCompatActivity() {
                     // Permission granted, start accessing location
                     getLocation()
                 } else {
-                    // Permission denied, handle accordingly (e.g., show a message)
-                    // You might want to inform the user why location access is needed
+                    Toast.makeText(this,
+                        "Location permission denied. Please grant permission to access your location.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
@@ -73,8 +77,11 @@ class GetLocationActivity : AppCompatActivity() {
                         val latitude = it.latitude
                         val longitude = it.longitude
 
-                        Log.e("TAG", latitude.toString())
-                        Log.e("TAG", longitude.toString())
+                        val resultIntent = Intent()
+                        resultIntent.putExtra("latitude", latitude)
+                        resultIntent.putExtra("longitude", longitude)
+                        setResult(Activity.RESULT_OK, resultIntent)
+                        finish()
                     }
                 }
         }
